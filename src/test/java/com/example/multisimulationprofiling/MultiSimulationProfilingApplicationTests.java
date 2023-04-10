0b6259976a2c78d04f22d0a5ff8fe950d7e73f90
Omar Atef <20190356@stud.fci-cu.edu.eg>
2023-04-10 13:29:47 +0200

    initial Commit

---
+++ src/test/java/com/example/multisimulationprofiling/MultiSimulationProfilingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multisimulationprofiling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiSimulationProfilingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
