a26b338283b64fb7ddec79e15e1aed28a0a65dda
Omar Atef <20190356@stud.fci-cu.edu.eg>
2023-04-13 15:01:41 +0200

    new project wit hspring 2.7 and graddle

---
+++ Multi-Simulation-Profiling-Analysis-Backend/src/main/java/com/example/multisimulationprofilinganalysisbackend/MultiSimulationProfilingAnalysisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.multisimulationprofilinganalysisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiSimulationProfilingAnalysisBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiSimulationProfilingAnalysisBackendApplication.class, args);
+    }
+
+}
