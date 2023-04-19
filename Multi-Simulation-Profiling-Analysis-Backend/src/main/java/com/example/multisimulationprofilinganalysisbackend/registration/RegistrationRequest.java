c7a2ef7cf100ea7bbc3ae5e72df4f0974668245a
omar awad <awadafif77777@gmail.com>
2023-04-19 16:22:41 +0200

    Finishing the login/registration and security system

---
+++ Multi-Simulation-Profiling-Analysis-Backend/src/main/java/com/example/multisimulationprofilinganalysisbackend/registration/RegistrationRequest.java
@@ -0,0 +1,51 @@
+package com.example.multisimulationprofilinganalysisbackend.registration;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+    private  final String firstName;
+    private final String lastName;
+    private  final String email;
+    private  final String password;
+
+    public RegistrationRequest(String firstName , String lastName, String email,String password ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+
+
+    }
+    public String  getEmail() {
+        return email;
+    }
+
+    public String  getFirstName() {
+        return firstName;
+    }
+
+    public String  getLastName() {
+        return lastName;
+    }
+
+    public String  getPassword() {
+        return password;
+    }
+
+  /*  public String  getConfirmedAt() {
+        return "2023";
+    }
+
+    public String  getExpiresAt() {
+        return "2030";
+    }
+    public String  getAppUser() {
+        return "App user";
+    }*/
+
+}
