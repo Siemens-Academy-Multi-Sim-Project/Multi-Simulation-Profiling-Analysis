80856b424909ab209f3860efa35930de8a6beb2d
sara-montasser <saramontasser4@gmail.com>
2023-05-04 20:38:57 +0300

    adding clusters table

--- src/main/java/com/example/multisimulationprofilinganalysisbackend/model/ProfilingData.java
+++ src/main/java/com/example/multisimulationprofilinganalysisbackend/model/ProfilingData.java
@@ -101,6 +101,10 @@ public class ProfilingData {
     @OneToMany(mappedBy = "profilerId", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<DesignUnit> designUnits = new ArrayList<>();
 
+    @OneToOne
+    @JoinColumn(name="ProfilingDataClusterID", referencedColumnName = "id")
+    private profilingDataClusters profilingDataCluster;
+
     public Long getId() {
         return id;
     }
@@ -275,5 +279,12 @@ public class ProfilingData {
 
     public void setDesignUnits(List<DesignUnit> designUnits) {
         this.designUnits = designUnits;
+    }
+    public profilingDataClusters getProfilingDataCluster() {
+        return profilingDataCluster;
+    }
+
+    public void setProfilingDataCluster(profilingDataClusters profilingDataCluster) {
+        this.profilingDataCluster = profilingDataCluster;
     }
 }
---
+++ src/main/java/com/example/multisimulationprofilinganalysisbackend/model/profilingDataClusters.java
@@ -0,0 +1,55 @@
+package com.example.multisimulationprofilinganalysisbackend.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.UniqueElements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ProfilingDataClusters")
+public class profilingDataClusters {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    
+    
+    @Column(name = "ClusterID")
+    @NotNull
+    private String clusterID;
+    
+    @Column(name = "ClusterName")
+    @NotNull
+    @UniqueElements
+    private String clusterName;
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    
+    public String getClusterID() {
+        return clusterID;
+    }
+
+    public void setClusterID(String clusterID) {
+        this.clusterID = clusterID;
+    }
+
+    public String getClusterName() {
+        return clusterName;
+    }
+
+    public void setClusterName(String clusterName) {
+        this.clusterName = clusterName;
+    }
+    
+}
