4d1150b5996bdd3bf062b79ac3c345cadba3657c
Omar Atef <20190356@stud.fci-cu.edu.eg>
2023-05-07 23:36:34 +0200

    meh

--- src/main/java/com/example/multisimulationprofilinganalysisbackend/controller/ProfilingDataController.java
+++ src/main/java/com/example/multisimulationprofilinganalysisbackend/controller/ProfilingDataController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
 @RestController
 public class ProfilingDataController {
     @Autowired
