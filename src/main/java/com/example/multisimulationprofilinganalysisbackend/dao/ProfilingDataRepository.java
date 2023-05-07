2177ff81a1c91e4603c8aed281ae64d8f8075d85
Omar Atef <20190356@stud.fci-cu.edu.eg>
2023-05-07 17:13:26 +0200

    hotfix again

--- src/main/java/com/example/multisimulationprofilinganalysisbackend/dao/ProfilingDataRepository.java
+++ src/main/java/com/example/multisimulationprofilinganalysisbackend/dao/ProfilingDataRepository.java
@@ -11,9 +11,9 @@ import java.util.List;
 
 @Repository
 public interface ProfilingDataRepository extends JpaRepository<ProfilingData, Long> {
-    @Query(value = "SELECT COUNT(*) FROM (SELECT design_Composition_Name, design_Composition_Modules,design_Composition_Packages,design_Composition_Interfaces,design_Composition_Instances FROM Profiling_Data GROUP BY design_Composition_Name, design_Composition_Modules,design_Composition_Packages,design_Composition_Interfaces,design_Composition_Instances HAVING COUNT(*) > 0) AS sub", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM (SELECT designcompositionname, designcompositionmodules,designcompositionpackages,designcompositioninterfaces,designcompositioninstances FROM profilingdata GROUP BY designcompositionname, designcompositionmodules,designcompositionpackages,designcompositioninterfaces,designcompositioninstances HAVING COUNT(*) > 0) AS sub", nativeQuery = true)
     String getDesignsNumber();
 
-    @Query(value = "SELECT * FROM Profiling_Data WHERE Profiling_Data.profiling_data_clusterid = :clusterId", nativeQuery = true)
+    @Query(value = "SELECT * FROM profilingdata WHERE profilingdata.profilingdataclusterid = :clusterId", nativeQuery = true)
     List<ProfilingData> findProfilingDatas(@Param("clusterId") Long clusterID);
 }
