c5c2df407550b59798a55c7d1bca2d2af2f067d4
Peter Atef <peter.atef@coformatique.com>
2023-05-10 19:24:38 +0200

    added custom trim function

--- src/main/java/com/example/multisimulationprofilinganalysisbackend/utils/StringUtils.java
+++ src/main/java/com/example/multisimulationprofilinganalysisbackend/utils/StringUtils.java
@@ -3,16 +3,30 @@ package com.example.multisimulationprofilinganalysisbackend.utils;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
+
 public class StringUtils {
     public static boolean containsIgnoreCase(String str, String substr) {
         return str.toLowerCase().contains(substr.toLowerCase());
     }
 
+    private static Boolean isIgnored(char c){ return Character.isWhitespace(c) || c == '"' || c == ' ';}
+
+    public static String trim(String in){
+        StringBuilder returnStr = new StringBuilder("");
+        for(int i = 0; i < in.length(); i++){
+            if(isIgnored(in.charAt(i))) continue;
+            
+            returnStr.append(in.charAt(i));
+        }
+
+        return returnStr.toString();
+    }
+
     public static String[] trim(String[] arr){
         ArrayList<String> trimmedArr = new ArrayList<>();
         for(int i = 0; i < arr.length; i++){
-            if(arr[i].isEmpty() || arr[i].isBlank() || arr[i].equals("\"")) continue;
-            trimmedArr.add(arr[i].trim());
+            if(arr[i].isEmpty() || arr[i].isBlank()) continue;
+            trimmedArr.add(trim(arr[i]));
         }
         return trimmedArr.toArray(new String[0]);
     }
